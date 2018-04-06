3437cd39d9e03ac1aeca890bc9ceb0dc3f5f744f
Randy Souza <rsouza@rsouza-maci.dhcp.mathworks.com>
2018-04-06 16:45:39 -0400

    Update dot.bash

--- dot.bash
+++ dot.bash
@@ -1,13 +1,13 @@
 SOURCE_PATH=$(pwd);
 
 ln -s $SOURCE_PATH/ackrc $HOME/.ackrc;
+ln -s $SOURCE_PATH/atom/config.cson $HOME/.atom/config.cson
+ln -s $SOURCE_PATH/atom/keymap.cson $HOME/.atom/keymap.cson
+ln -s $SOURCE_PATH/atom/styles.less $HOME/.atom/styles.less
 ln -s $SOURCE_PATH/bash_aliases $HOME/.bash_aliases;
 ln -s $SOURCE_PATH/bash_profile $HOME/.bash_profile;
 ln -s $SOURCE_PATH/bashrc $HOME/.bashrc;
 ln -s $SOURCE_PATH/gemrc $HOME/.gemrc;
-ln -s $SOURCE_PATH/hunspell_en_US $HOME/.hunspell_en_US;
 ln -s $SOURCE_PATH/inputrc $HOME/.inputrc;
 ln -s $SOURCE_PATH/irbrc $HOME/.irbrc;
 ln -s $SOURCE_PATH/pryrc $HOME/.pryrc;
-ln -s $SOURCE_PATH/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
-ln -s $SOURCE_PATH/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
