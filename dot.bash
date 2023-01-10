93077bccc966165b1a5c0c8fd94f4fce65cd51fc
Randy Souza <rsouza@mathworks.com>
2023-01-10 18:29:07 -0500

    Fix bashrc link

--- dot.bash
+++ dot.bash
@@ -3,8 +3,9 @@ __source_path=$(pwd)
 ln -s $__source_path/bash_aliases $HOME/.bash_aliases
 ln -s $__source_path/bash_profile $HOME/.bash_profile
 ln -s $__source_path/bash_prompt $HOME/.bash_prompt
-ln -s $__source_path/bashrc $HOME/.bashrc
+ln -s $__source_path/bashrc $HOME/.bashrc.mine
 ln -s $__source_path/gemrc $HOME/.gemrc
 ln -s $__source_path/inputrc $HOME/.inputrc
 ln -s $__source_path/irbrc $HOME/.irbrc
 ln -s $__source_path/pryrc $HOME/.pryrc
+
